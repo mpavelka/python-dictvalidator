a52e4410cfa4be5666b65f9194211ed713c31fd4
Miloslav Pavelka <pavelkamiloslav@gmail.com>
2017-08-18 13:35:17 +0200

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name='dictvalidator',
+      version='0.0.1',
+      description='Validator for a python dictionary. Provides basic validation and is extensible with custom validators.',
+      url='https://github.com/mpavelka/python-dictvalidator',
+      author='Miloslav Pavelka',
+      author_email='pavelkamiloslav@gmail.com',
+      license='BSD-3',
+      packages=find_packages(),
+      zip_safe=False)
